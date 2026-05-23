ca058f961c2030e8342ee73b174497c32734ace2
Griffin84F <christianlimoni84@gmail.com>
2026-05-23 10:08:22 -0300

    feature: validacao de idade

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 
 n = input('Digite seu nome:')
 i =int(input('Digite sua idade: \n'))
+
+if i >=18:
+    print('cadastrado com sucesso')
+else:
+    print('Só pode ser cadastrado, pessoas com idade superior a 18 anos')
+
